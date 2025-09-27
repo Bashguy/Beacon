a05745b219332b08418a096b85057a185a6a023d
rockbison1230 <amanamartin8@gmail.com>
2025-09-27 13:58:24 -0400

    added andada font + beacon logo img

--- Frontend/beacon/app/index.tsx
+++ Frontend/beacon/app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">yesssirrr</Text>
+    <View className="flex-1 items-center justify-center bg-[#252D1F]">
+      <Text className="text-xl font-[Andada] text-[#F4E3CC]">Beacon</Text>
     </View>
   );
 }
